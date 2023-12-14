71edf68c27d4942c43b7418f165e4604338f24e0
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-14 13:39:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 const parseCode = (str) => {
-  // your code here
-};
+  let ans = {};
+  let index = 0;
 
-// Do not change the code below
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '0') {
+      ans["firstName"] = str.slice(0, i);
+      index = i;
+      break;
+    }
+  }
+
+  let index2 = index;
+
+  for (let i = index; i < str.length; i++) {
+    if (('a' <= str[i] && str[i] <= 'z') || ('A' <= str[i] && str[i] <= 'Z')) {
+      index2 = i;
+		break; 
+    }
+  }
+
+  let index3 = index2;
+
+  for (let i = index2; i < str.length; i++) {
+    if (('a' <= str[i] && str[i] <= 'z') || ('A' <= str[i] && str[i] <= 'Z')) {
+      ans["lastName"] = str.slice(index2, i+1);
+      index3 = i;
+    }  
+  }
+  let index4 = index3+1;
+
+  for (let i = index3; i < str.length; i++) {
+    if (str[i]>='1' && str[i] <= '9'){
+		index4 = i; 
+		break;
+	}
+	  
+  }
+  for (let i = index4; i < str.length; i++) {
+    if ('0' <= str[i] && str[i] <= '9') {
+      ans["id"] = str.slice(index4, str.length);
+    } 
+  }
+
+  return ans;
+}; 
+  
+// Example usage:
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
